1cd5fbddba92b5016d55684371d076702bdcb2fd
Heverson Lima <81335621+heversonlima@users.noreply.github.com>
2023-05-23 15:04:20 -0300

    Create aula6.py

---
+++ aulas/aula6.py
@@ -0,0 +1,10 @@
+# condicionais if, elif e else
+
+idade = int(input('Informe sua idade: '))
+
+if idade > 18:
+    print('Maior de idade: ')
+elif idade > 16:
+    print('Infanto juvenil')
+else:
+    print('Menor de idade')
